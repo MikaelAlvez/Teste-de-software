cc7beb6b699e06e48da71948ce07a39dc48625e2
lucas vinicius <lucassilved@gmail.com>
2025-05-22 12:40:58 -0300

    feat: adiciona mais instruções

--- src/jumpingthings/App.java
+++ src/jumpingthings/App.java
@@ -7,6 +7,14 @@ public class App {
     private final static int MAXIMUM_MATCH_DURATION = 100;
 
     public static void main(String[] args) {
+
+        /*
+        * A simulação para quando chega no número de MAXIMUM_MATCH_DURATION
+        * Ou quando a metade das criaturas chegam em 1 de coins por: match.hasHalfElementsReachedOneCoin()
+        * - Possíveis erro
+        *   - PACKAGE errado
+        * */
+
         final var match = new Match(30);
         final var panel = new VisualizationPanel(match);
         final var frame = new JFrame("Simulação de Criaturas Saltitantes");
